fc474bdc21f4759cab7de0acf4459b8fa855440e
outlau <la-bobtheko@hotmail.com>
2022-03-10 20:41:31 +0100

    add cap

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hacker.wordle',
+  appName: 'wordle',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
